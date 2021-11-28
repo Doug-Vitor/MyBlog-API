c0ebc256f34a577588262aaa5120f7e1709cdc74
Douglas Vitor <douglasvitor.dev@gmail.com>
2021-11-27 21:36:23 -0300

    Password Validation Attribute

---
+++ src/BetterNews.ApplicationCore/ViewModels/Validations/PasswordValidationAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+public class PasswordValidationAttribute : ValidationAttribute
+{
+    private Regex RequiredChars = new (@"(?=.*[a-zA-Z0-9])(?=.*[-+_!@#$%^&*.,?])");
+    private Regex RequiredLength = new(@".{8,}");
+
+    protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+        => (RequiredChars.IsMatch((string)value) && RequiredLength.IsMatch((string)value)) ? ValidationResult.Success
+        : new ValidationResult("A senha deve conter pelo menos uma letra minúscula, uma letra maiúscula, um caractere especial e, no total, 8 (oito) caracteres.");
+}
